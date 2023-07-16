dd1a8aae112efa0b5acc8f0ba5955a66ff655e8d
Suraj Hanvate <surajhanvate999@gmail.com>
2023-07-16 17:16:01 +0530

    added client side validations

--- src/main/resources/static/js/expense-form.js
+++ src/main/resources/static/js/expense-form.js
@@ -1,3 +1,12 @@
+let validNumber = new RegExp(/^(?!0)\d*\.?\d{0,2}$/);
+let amountValid = $("#amount").val();
+const validateNumber = function(elem) {
+	if(validNumber.test(elem.value)) {
+		amountValid = elem.value;
+	} else {
+		elem.value = amountValid;
+	}
+}
 $(function() {
 	$('#createdAt').datepicker({
 		dateFormat: "dd/mm/yy",
@@ -6,3 +15,34 @@ $(function() {
 		maxDate: new Date()
 	});
 })
+
+const $expenseForm = $("#expenseForm");
+
+if($expenseForm.length) {
+	$expenseForm.validate({
+		rules: {
+			name:{
+				required: true,
+				minlength: 3
+			},
+			amount:{
+				required: true
+			},
+			dateString:{
+				required: true
+			}
+		},
+		messages: {
+			name: {
+				required: 'Please enter the expense name',
+				minlength: 'Expense name should be minimum 3 characters'
+			},
+			amount: {
+				required: 'Please enter the amount'
+			},
+			dateString: {
+				required: 'Please enter the date'
+			}
+		}
+	})
+}
