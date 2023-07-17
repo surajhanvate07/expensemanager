d76f0fba16eac922570432f0aaf73928fcdc6b3a
Suraj Hanvate <surajhanvate999@gmail.com>
2023-07-17 11:51:30 +0530

    added validations to login form

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,25 @@
+$(function() {
+
+	const $loginForm = $("#loginForm");
+
+	if($loginForm.length) {
+		$loginForm.validate({
+			rules: {
+				email:{
+					required: true
+				},
+				password:{
+					required: true
+				}
+			},
+			messages: {
+				email: {
+					required: 'Please enter the email'
+				},
+				password: {
+					required : 'Please enter the password'
+				}
+			}
+		})
+	}
+})
